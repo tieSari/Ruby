77d567135defb2380cea7f2740f8ecf3876487ba
Sari R <sariraut@dhcp-asv-101.helsinki.fi>
2015-01-23 22:55:54 +0200

    viikko2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :beers
-
   resources :breweries
+  root 'breweries#index'
+  resources :ratings, only: [:index, :new, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
