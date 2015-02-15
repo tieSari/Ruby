70fa7d3c08bbc6c7b29b776133a618a2d421f422
Sari R <sariraut@dhcp-asv-101.helsinki.fi>
2015-02-15 21:52:23 +0200

    korjauksia viikko 5

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -55,7 +55,7 @@ class MembershipsController < ApplicationController
   # DELETE /memberships/1
   # DELETE /memberships/1.json
   def destroy
-    @membership = current_user.memberships.find(@membership.__id__)
+    @membership = current_user.memberships.find(@membership.__id)
     @membership.destroy
     respond_to do |format|
       format.html { redirect_to user_path(current_user.id), notice: 'Membership was successfully destroyed.' }
--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -5,11 +5,11 @@ class RatingsController < ApplicationController
 	def index
 		@ratings = Rating.all
 
-		@recent = Rating.recent 5
-		@top_breweries = Brewery.top 3
-		@top_beers = Beer.top 3
-		@top_users = User.top 3
-		@top_styles = Style.top 3
+	#	@recent = Rating.recent 5
+	#	@top_breweries = Brewery.top 3
+	#	@top_beers = Beer.top 3
+	#	@top_users = User.top 3
+	#	@top_styles = Style.top 3
  	end
 
 	def new
